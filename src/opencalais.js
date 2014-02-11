6a2df84b0f830ca249b01187dcf7fb45858621c7
Mark Watson <markw@markwatson.com>
2014-02-11 10:52:35 -0800

    added Open Calais example

--- src/opencalais.js
+++ src/opencalais.js
@@ -1,6 +1,17 @@
-/**
- * Created by markw on 2/10/14.
- */
+var calais = require ("calais");
 
-// TBD
+var open_calais_api_key = process.env.OPEN_CALAIS_KEY;
+
+var i, calais_client = new calais.Calais(open_calais_api_key);
+
+calais_client.set('content', 'President Obama spoke before Congress about taxes and income inequality.');
+
+calais_client.fetch(function(results) {
+  for (i = 0; i < results.length; i += 1) {
+    //console.log(results[i]);
+    if (results[i]['_type'] && results[i]['name']) {
+      console.log(results[i]['_type'] + " " + results[i]['name']);
+    }
+  }
+});
 
